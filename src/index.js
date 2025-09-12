71b24c24e83b2a8324bc6b2c72dec3b47a1eb16e
Serhii Bratkov <sir3720@gmail.com>
2025-09-12 14:06:56 +0300

    fix4

--- src/index.js
+++ src/index.js
@@ -6,27 +6,17 @@ const PORT = 3000;
 
 const app = express();
 
-// Middleware для логування часу запиту
-app.use((req, res, next) => {
-  console.log(`Time: ${new Date().toLocaleString()}`);
-  next();
-});
-
-// Вбудований у express middleware для обробки (парсингу) JSON-даних у запитах
-// наприклад, у запитах POST або PATCH
 app.use(express.json());
 
-// Маршрут для обробки GET-запитів на '/'
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello, World!",
+    message: "Hello World!",
   });
 });
 
-// Middleware для обробких помилок (приймає 4 аргументи)
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: "Something went wrong",
+app.use("*", (req, res, next) => {
+  res.status(404).json({
+    message: "Not found",
   });
 });
 
