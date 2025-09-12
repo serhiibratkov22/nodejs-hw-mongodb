bcee9e5d37bccf00bf9f2b0b7cf6c1667c0e17ab
Serhii Bratkov <sir3720@gmail.com>
2025-09-12 14:14:05 +0300

    fix5

--- src/index.js
+++ src/index.js
@@ -10,13 +10,20 @@ app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello World!",
+    message: "Hello world!",
   });
 });
 
 app.use("*", (req, res, next) => {
   res.status(404).json({
     message: "Not found",
+  });
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).json({
+    message: "Something went wrong",
+    error: err.message,
   });
 });
 
