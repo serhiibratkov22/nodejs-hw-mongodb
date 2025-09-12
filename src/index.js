742f234174a278c64b6d5bfbc3cdd6d7df7fb4ac
Serhii Bratkov <sir3720@gmail.com>
2025-09-12 22:56:39 +0300

    add server

--- src/index.js
+++ src/index.js
@@ -1,32 +1,5 @@
 // src/index.js
 
-import express from "express";
+import { startServer } from "./server.js";
 
-const PORT = 3000;
-
-const app = express();
-
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.json({
-    message: "Hello world!",
-  });
-});
-
-app.use("*", (req, res, next) => {
-  res.status(404).json({
-    message: "Not found",
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: "Something went wrong",
-    error: err.message,
-  });
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer();
