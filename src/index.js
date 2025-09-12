beb39a301ae4244e3b8e8dfc8c68524e3280bd6f
Serhii Bratkov <sir3720@gmail.com>
2025-09-12 12:20:57 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@
 
 import express from "express";
 
-const app = express();
-
 const PORT = 3000;
 
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello world!",
+  });
+});
+
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on ${PORT}`);
 });
