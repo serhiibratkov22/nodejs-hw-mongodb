41df234977cb2374d05728dce02959938483f540
Serhii Bratkov <sir3720@gmail.com>
2025-09-14 16:41:58 +0300

    add utils

--- src/server.js
+++ src/server.js
@@ -4,7 +4,9 @@ import express from "express";
 import pino from "pino-http";
 import cors from "cors";
 
-const PORT = 3000;
+import { getEnvVar } from "./utils/getEnvVar.js";
+
+const PORT = Number(getEnvVar("PORT", "3000"));
 
 export const startServer = () => {
   const app = express();
@@ -22,18 +24,16 @@ export const startServer = () => {
 
   app.get("/", (req, res) => {
     res.json({
-      message: "Hello world!",
+      message: "Hello World!",
     });
   });
 
-  // універсальний обробник 404
-  app.use((req, res) => {
+  app.use("*", (req, res, next) => {
     res.status(404).json({
       message: "Not found",
     });
   });
 
-  // обробник помилок
   app.use((err, req, res, next) => {
     res.status(500).json({
       message: "Something went wrong",
