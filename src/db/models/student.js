e216a2ee605dd14316e6bdf51d1918fcae7b3d98
Serhii Bratkov <sir3720@gmail.com>
2025-09-15 23:56:16 +0300

    add db

---
+++ src/db/models/student.js
@@ -0,0 +1,34 @@
+// src/db/models/student.js
+
+import { Schema } from "mongoose";
+
+const studentsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female", "other"],
+    },
+    avgMark: {
+      type: Number,
+      required: true,
+    },
+    onDuty: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  }
+);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 // src/index.js
 
+import { initMongoDB } from "./db/initMongoDB.js";
 import { startServer } from "./server.js";
 
-startServer();
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
+
+bootstrap();
